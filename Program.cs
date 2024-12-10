a78a178adc440d738a3c1abaf0acb28261eedc3c
hayadid <yalibrotman@gmail.com>
2024-12-10 16:39:20 +0200

    copied and translated basic camera template
    "basic camera free" on the raylib website. translated from c to c#.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 
 public static class Program
 {
@@ -8,19 +9,35 @@ public static class Program
         //--------------------------------------------------------------------------------------
         const int screenWidth = 800;
         const int screenHeight = 450;
-        const string poop = "poop";
 
         Raylib.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
         Raylib.SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
                                                //--------------------------------------------------------------------------------------
 
+        // Define the camera to look into our 3d world
+        Camera3D camera = new Camera3D();
+        camera.Position = new Vector3(10.0f, 10.0f, 10.0f); // Camera position
+        camera.Target = Vector3.Zero;                        // Camera looking at point
+        camera.Up = new Vector3(0.0f, 1.0f, 0.0f);          // Camera up vector (rotation towards target)
+        camera.FovY = 45.0f;                                // Camera field-of-view Y
+        camera.Projection = CameraProjection.Perspective;   // Camera projection type
+
+        Vector3 cubePosition = Vector3.Zero;
+
+        Raylib.DisableCursor();                    // Limit cursor to relative movement inside the window
+
+        Raylib.SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+                                                   //--------------------------------------------------------------------------------------
+
         // Main game loop
-        while (!Raylib.WindowShouldClose())    // Detect window close button or ESC key
+        while (!Raylib.WindowShouldClose())        // Detect window close button or ESC key
         {
             // Update
             //----------------------------------------------------------------------------------
-            // TODO: Update your variables here
+            Raylib.UpdateCamera(ref camera, CameraMode.Free);
+
+            if (Raylib.IsKeyPressed(KeyboardKey.Z)) camera.Target = Vector3.Zero;
             //----------------------------------------------------------------------------------
 
             // Draw
@@ -29,7 +46,22 @@ public static class Program
 
             Raylib.ClearBackground(Color.RayWhite);
 
-            Raylib.DrawText("Congrats! You created your first window!", 190, 200, 20, Color.LightGray);
+            Raylib.BeginMode3D(camera);
+
+            Raylib.DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, Color.Red);
+            Raylib.DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, Color.Maroon);
+
+            Raylib.DrawGrid(10, 1.0f);
+
+            Raylib.EndMode3D();
+
+            Raylib.DrawRectangle(10, 10, 320, 93, Raylib.Fade(Color.SkyBlue, 0.5f));
+            Raylib.DrawRectangleLines(10, 10, 320, 93, Color.Blue);
+
+            Raylib.DrawText("Free camera default controls:", 20, 20, 10, Color.Black);
+            Raylib.DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, Color.DarkGray);
+            Raylib.DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, Color.DarkGray);
+            Raylib.DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, Color.DarkGray);
 
             Raylib.EndDrawing();
             //----------------------------------------------------------------------------------
@@ -39,7 +71,5 @@ public static class Program
         //--------------------------------------------------------------------------------------
         Raylib.CloseWindow();        // Close window and OpenGL context
                                      //--------------------------------------------------------------------------------------
-        
-        return;
     }
 }
